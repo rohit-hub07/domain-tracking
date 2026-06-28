7d6b68340137e568182ec31c5ff966e73d2b8505
rohitsingh <rohitsingh986491@gmail.com>
2026-06-28 22:01:36 +0530

    fix:implemented redis cache in profile route

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,18 +1,26 @@
 import { dbConnection } from "@/src/db/dbConnection";
+import { redis } from "@/src/lib/redis";
+import { getDataFromJwt } from "@/src/utilities/getDataFromJwt";
 import dotenv from "dotenv"
 import { NextRequest, NextResponse } from "next/server";
 
 dotenv.config();
 
-export async function GET(request: NextRequest){
+export async function GET(request: NextRequest) {
   try {
     await dbConnection()
     const response = NextResponse.json({
       message: "User logged out successfully!",
       success: true,
-    },{status: 200})
+    }, { status: 200 })
+
+    const tokenData = request.cookies.get("token")?.value
 
     response.cookies.delete("token")
+    
+    if (tokenData) {
+      await redis.del(`user_session:${tokenData}`)
+    }
 
     return response;
 
@@ -20,6 +28,6 @@ export async function GET(request: NextRequest){
     return NextResponse.json({
       message: error.message,
       success: false
-    },{status: 500})
+    }, { status: 500 })
   }
 }
--- src/utilities/getDataFromJwt.ts
+++ src/utilities/getDataFromJwt.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 import { NextRequest } from "next/server"
+import { redis } from "../lib/redis"
 
 interface CustomJWt{
   id: string,
@@ -8,10 +9,17 @@ interface CustomJWt{
 
 export async function getDataFromJwt(request:NextRequest){
   try {
+    
     const tokenData = request.cookies.get("token")?.value
-
+    
     if(!tokenData){
-      throw new Error("Token not found!")
+      throw new Error("Please login!")
+    }
+
+    const cachedData = await redis.get(`user_session:${tokenData}`);
+    if(cachedData){
+      console.log("cacheddata: ",cachedData)
+      return JSON.parse(cachedData) as CustomJWt;
     }
 
     const decodedData = jwt.verify(tokenData, process.env.JWT_SECRET as string) as CustomJWt
@@ -20,8 +28,11 @@ export async function getDataFromJwt(request:NextRequest){
       throw new Error("Invalid token payload structure");
     }
 
+    await redis.set(`user_session:${tokenData}`,JSON.stringify(decodedData),"EX",3600);
+
     return decodedData
   } catch (error:any) {
+    console.log("error inside of getDatafrom jwt: ",error.message)
     throw new Error(error.message);
   }
 }
